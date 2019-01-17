31ff09b91b3270c8de839a2b37e964e3db06ba03
greg higgins <greg.higgins@v12technology.com>
2019-01-17 22:00:53 +0000

    added simple string cache for interning CharSequence

---
+++ extensions/declarative/api/src/test/java/com/fluxtion/ext/declarative/api/util/StringCacheTest.java
@@ -0,0 +1,38 @@
+package com.fluxtion.ext.declarative.api.util;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import org.junit.Assert;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+/**
+ * 
+ * @author gregp
+ */
+public class StringCacheTest {
+
+    @Test
+    public void cacheTest() {
+        StringBuilder sb_0 = new StringBuilder("sb_0");
+        StringBuilder sb_1 = new StringBuilder("sb_1");
+        StringBuilder sb_2 = new StringBuilder("sb_2");
+        StringBuilder sb_22 = new StringBuilder("sb_2");
+
+        StringCache cache = new StringCache();
+        assertThat(sb_0, is(not(sb_1)));
+        assertThat(sb_2, is(not(sb_22)));
+        assertThat(sb_2.toString(), is((sb_22.toString())));
+        
+        cache.intern(sb_0);
+        cache.intern(sb_0);
+        cache.intern(sb_0);
+        assertThat(1, is(cache.cacheSize()));
+        
+        CharSequence sb_2_String = cache.intern(sb_2);
+        CharSequence sb_22_String = cache.intern(sb_22);
+        assertThat(2, is(cache.cacheSize()));
+        
+        Assert.assertTrue(sb_2_String==sb_22_String);
+    }
+}
