4dc6ecae6e3c934c1b2323cb0e8787366fe80597
greg higgins <greg.higgins@v12technology.com>
2019-01-18 12:55:53 +0000

    dependencies and imports

--- examples/futext-examples/src/test/java/com/fluxtion/ext/futext/example/flightdelay/FlightDelayTest.java
+++ examples/futext-examples/src/test/java/com/fluxtion/ext/futext/example/flightdelay/FlightDelayTest.java
@@ -33,7 +33,6 @@ import static com.fluxtion.generator.compiler.InprocessSepCompiler.DirOptions.JA
 import static com.fluxtion.generator.compiler.InprocessSepCompiler.InitOptions.INIT;
 import com.fluxtion.generator.util.ClassUtils;
 import java.io.BufferedOutputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
