eb090e4b58c309097fff8cd07fa67e89d34314e8
greg higgins <greg.higgins@v12technology.com>
2019-01-31 23:35:53 +0000

    annotations for annotation processing during FLuxtion generation

---
+++ builder/src/main/java/com/fluxtion/builder/annotation/Disabled.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2019 V12 Technology Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by MongoDB, Inc.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Server Side License for more details.
+ *
+ * You should have received a copy of the Server Side Public License
+ * along with this program.  If not, see 
+ * <http://www.mongodb.com/licensing/server-side-public-license>.
+ */
+package com.fluxtion.builder.annotation;
+
+import java.lang.annotation.Documented;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+import java.lang.annotation.Target;
+
+/**
+ * Used in conjunction with generation annotations to allow the developer to
+ * conditionally enable/disable processing of the annotation in context.
+ * @author V12 Technology Ltd. 
+ */
+@Target(value = {TYPE, METHOD})
+ @Retention(value=CLASS)
+ @Documented
+public @interface Disabled {
+    
+}
