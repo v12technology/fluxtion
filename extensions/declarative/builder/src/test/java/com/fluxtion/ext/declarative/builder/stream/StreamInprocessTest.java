9e25d26386951d1ebe9612b1d2d6d01a8d77ca3f
greg higgins <greg.higgins@v12technology.com>
2019-03-25 21:40:05 +0000

    extends BasSepTest adds getWrappedField

--- extensions/declarative/builder/src/test/java/com/fluxtion/ext/declarative/builder/stream/StreamInprocessTest.java
+++ extensions/declarative/builder/src/test/java/com/fluxtion/ext/declarative/builder/stream/StreamInprocessTest.java
@@ -17,74 +17,20 @@
  */
 package com.fluxtion.ext.declarative.builder.stream;
 
-import com.fluxtion.api.event.Event;
-import com.fluxtion.api.lifecycle.EventHandler;
-import com.fluxtion.api.lifecycle.Lifecycle;
-import com.fluxtion.builder.node.SEPConfig;
-import static com.fluxtion.generator.compiler.InprocessSepCompiler.sepTestInstance;
-import java.util.function.Consumer;
+import com.fluxtion.ext.declarative.api.Wrapper;
+import com.fluxtion.generator.util.BaseSepInprocessTest;
 import net.vidageek.mirror.dsl.Mirror;
-import org.junit.Before;
-import org.junit.Rule;
-import org.junit.rules.TestName;
 
 /**
  *
  * @author Greg Higgins greg.higgins@v12technology.com
  */
-public class StreamInprocessTest {
+public class StreamInprocessTest extends BaseSepInprocessTest{
 
-    protected EventHandler sep;
-    protected boolean fixedPkg = false;
-    @Rule
-    public TestName testName = new TestName();
-
-    @Before
-    public void beforeTest() {
-        fixedPkg = false;
-    }
-
-    protected EventHandler sep(Class<? extends EventHandler> handlerClass) {
-        try {
-            sep = handlerClass.newInstance();
-            if(sep instanceof Lifecycle){
-                ((Lifecycle)sep).init();
-            }
-            return sep;
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
+    protected <T> T getWrappedField(String name) {
+        Wrapper<T> wrapped = (Wrapper<T>) new Mirror().on(sep).get().field(name);
+        return wrapped.event();
     }
     
-    protected EventHandler sep(Consumer<SEPConfig> cfgBuilder) {
-        try {
-            sep = sepTestInstance(cfgBuilder, pckName(), sepClassName());
-            return sep;
-        } catch (Exception ex) {
-            throw new RuntimeException(ex);
-        }
-    }
-
-    protected String pckName() {
-        String pckg = this.getClass().getCanonicalName() + "_" + testName.getMethodName(); 
-        pckg = pckg.toLowerCase();
-        if (!fixedPkg) {
-            pckg += "_" + System.currentTimeMillis();
-        }
-        return pckg;
-    }
-
-    protected String sepClassName() {
-        return "TestSep_" + testName.getMethodName();
-    }
-
-    protected <T> T getField(String name) {
-        return (T) new Mirror().on(sep).get().field(name);
-    }
-
-    protected void onEvent(Event e) {
-        sep.onEvent(e);
-    }
-
 }
 
