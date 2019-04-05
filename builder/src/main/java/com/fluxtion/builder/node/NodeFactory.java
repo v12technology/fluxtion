ef2607401e809e565d6bc0fb0581cdcb9d7cbc08
greg higgins <greg.higgins@v12technology.com>
2019-04-05 14:32:19 +0100

    javadoc

--- builder/src/main/java/com/fluxtion/builder/node/NodeFactory.java
+++ builder/src/main/java/com/fluxtion/builder/node/NodeFactory.java
@@ -16,26 +16,31 @@
  */
 package com.fluxtion.builder.node;
 
+import com.fluxtion.api.annotations.Config;
+import com.fluxtion.api.annotations.Inject;
 import com.fluxtion.builder.generation.GenerationContext;
 import java.util.Map;
 import java.util.ServiceLoader;
 
 /**
  * A factory for creating instances. The SEP will call this method to create
- * nodes.
+ * nodes.<p>
  *
  * Instance re-use. To ensure that node instances are re-used there are two
  * approaches:
- *
- * 1. The factory caches the node, and returns the same instance for the same
- * configuration. 2. The factory creates new nodes, but the node overrides
- * hashcode and equals so interchangeable node instances will return true when
- * equality is tested. The framework will only use one of the instances and
- * ignore the subsequently created instances.
- * 
- * Fluxtion employs the {@link ServiceLoader} pattern to register NodeFactories.
- * Please read the java documentation describing the meta-data a factory implementor
- * must provide to register a factory using the {@link ServiceLoader} pattern.
+ * <ul>
+ * <li>The factory caches the node, and returns the same instance for the same
+ * configuration.
+ * <li>The factory creates new nodes, but the node overrides hashcode and equals
+ * so interchangeable node instances will return true when equality is tested.
+ * Will check an internal instance map of nodes and ignore the newly created
+ * node if hashcode and equals match.
+ * </ul>
+ * <p>
+ * Fluxtion employs the {@link ServiceLoader} pattern to register user
+ * implemented NodeFactories. Please read the java documentation describing the
+ * meta-data a factory implementor must provide to register a factory using the
+ * {@link ServiceLoader} pattern.
  *
  * @author Greg Higgins
  * @param <T>
@@ -43,37 +48,46 @@ import java.util.ServiceLoader;
 public interface NodeFactory<T> {
 
     /**
+     * NodeFactory writer must implement this method to generate instances of
+     * nodes. The Fluxtion compiler will call this method when an {@link Inject}
+     * instance is created. {@link Config} variables are used to populate the
+     * config map.
      *
-     * @param config
-     * @param registry
+     * @param config map configuration
+     * @param registry The node registry of the current generation context
      * @return The newly created node instance
      */
     T createNode(Map config, NodeRegistry registry);
-    
-    /**
-     * 
-     * @param config
-     * @param registry
-     * @param instance 
-     */
-    default void postInstanceRegistration(Map config, NodeRegistry registry, T instance) { }
 
     /**
-     * 
+     * Callback invoked by Fluxtion generator after the generated SEP has been
+     * registered in the{@link GenerationContext}
+     *
+     * @param config map configuration
+     * @param registry The node registry of the current generation context
+     * @param instance the newly created instance
+     */
+    default void postInstanceRegistration(Map config, NodeRegistry registry, T instance) {
+    }
+
+    /**
+     *
      * If the node generates a class for this SEP, this callback will indicate
      * the desired target.
-     * 
-     * @param targetLanguage 
+     *
+     * @param targetLanguage target language for generation
      */
-    default public void setTargetLanguage(String targetLanguage) { }
+    default public void setTargetLanguage(String targetLanguage) {
+    }
 
     /**
-     * 
-     * If the node generates a class for this SEP, this callback gives 
-     * the node access to the GenerationContext. 
-     * 
-     * @param context 
+     *
+     * If the node generates a class for this SEP, this callback gives the node
+     * access to the GenerationContext before generation.
+     *
+     * @param context The context the Fluxtion SEC compiler uses
      */
-    default public void preSepGeneration(GenerationContext context) { }
+    default public void preSepGeneration(GenerationContext context) {
+    }
 
 }
