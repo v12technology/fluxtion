d6a40d0ced322cc588df8be59a470f12a50c2b73
greg higgins <greg.higgins@v12technology.com>
2019-04-15 21:58:41 +0100

    override return type for FilterWrapper

--- extensions/streaming/api/src/main/java/com/fluxtion/ext/streaming/api/FilterWrapper.java
+++ extensions/streaming/api/src/main/java/com/fluxtion/ext/streaming/api/FilterWrapper.java
@@ -1,5 +1,8 @@
 package com.fluxtion.ext.streaming.api;
 
+import com.fluxtion.api.partition.LambdaReflection;
+import com.fluxtion.api.partition.LambdaReflection.SerializableConsumer;
+import com.fluxtion.api.partition.LambdaReflection.SerializableFunction;
 import com.fluxtion.ext.streaming.api.stream.ElseWrapper;
 
 /**
@@ -21,6 +24,39 @@ public interface FilterWrapper<T> extends Wrapper<T>, Test {
     }
 
     @Override
-    public FilterWrapper<T> notifyOnChange(boolean notifyOnChange);
+    FilterWrapper<T> notifyOnChange(boolean notifyOnChange);
+
+    @Override
+    <T, R, S extends R> FilterWrapper<T> push(SerializableFunction<T, S> supplier, LambdaReflection.SerializableConsumer< R> mapper);
+
+    @Override
+    FilterWrapper<T> forEach(SerializableConsumer<T> consumer);
+
+    @Override
+    FilterWrapper<T> forEach(SerializableConsumer<T> consumer, String consumerId);
+
+    @Override
+    <S> FilterWrapper<T> console(String prefix, SerializableFunction<T, S>... supplier);
+
+    @Override
+    FilterWrapper<T> resetNotifier(Object resetNotifier);
+
+    @Override
+    FilterWrapper<T> notiferMerge(Object eventNotifier);
+
+    @Override
+    FilterWrapper<T> notifierOverride(Object eventNotifier);
+
+    @Override
+    FilterWrapper<T> publishAndReset(Object notifier);
+
+    @Override
+    FilterWrapper<T> immediateReset(boolean immediateReset);
+
+    @Override
+    FilterWrapper<T> alwaysReset(boolean alwaysReset);
+    
+    @Override
+    FilterWrapper<T> id(String id);
     
 }
