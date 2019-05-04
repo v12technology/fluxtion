4357883ad39acbd363ec266c8e349049259941fa
greg higgins <greg.higgins@v12technology.com>
2019-05-04 18:41:20 +0100

    updated license file PushBuilder added push a source instance to a target method Wrapper: 	use specific formal parameters, can now delete mapLong, mapInt and mapDouble methods 	added shortcut method where first argument of BiFunction accepts the wrapper type 	added push wrapped source instance to a target method
    updated tests

--- extensions/streaming/builder/src/test/java/com/fluxtion/ext/declarative/builder/push/Push_2Test.java
+++ extensions/streaming/builder/src/test/java/com/fluxtion/ext/declarative/builder/push/Push_2Test.java
@@ -11,6 +11,7 @@ import com.fluxtion.ext.declarative.builder.helpers.DealEvent;
 import com.fluxtion.ext.declarative.builder.helpers.TradeEvent;
 import com.fluxtion.ext.declarative.builder.stream.StreamInprocessTest;
 import com.fluxtion.ext.streaming.builder.stream.StreamBuilder;
+import static com.fluxtion.ext.streaming.builder.stream.StreamFunctionsBuilder.count;
 import java.util.Date;
 import static org.hamcrest.CoreMatchers.is;
 import org.junit.Assert;
@@ -85,6 +86,8 @@ public class Push_2Test extends StreamInprocessTest {
             PushBuilder.push(inDA, counter);
             //push data
             PushBuilder.push(counter::getCount, pushTarget::setVal);
+            PushBuilder.pushSource(counter, pushTarget::pushUpdateCount);
+            PushBuilder.pushSource(counter, pushTarget::setLongCount);
 
         });
 
@@ -97,18 +100,26 @@ public class Push_2Test extends StreamInprocessTest {
         Assert.assertThat(counter.count, is(3));
         Assert.assertThat(target.count, is(3));
         Assert.assertThat(target.val, is(30));
+        Assert.assertThat(target.updatePushVal, is(300));
+        Assert.assertThat(target.longVal, is(3000l));
     }
 
     @Test
     public void pushToComplexObject() {
         sep((c) -> {
             select(LongNumber.class)
-                    .push(LongNumber::getVal, c.addPublicNode(new Date(), "date")::setTime);
+                    .push(LongNumber::getVal, c.addPublicNode(new Date(), "date")::setTime)
+                    .map(count())
+                    .push(c.addPublicNode(new Date(), "date_2")::setTime);
         });
         
         Date date = getField("date");
+        Date date_2 = getField("date_2");
+        sep.onEvent(new LongNumber((8000)));
+        sep.onEvent(new LongNumber((2000)));
         sep.onEvent(new LongNumber((1000)));
         Assert.assertThat(date.getTime(), is(1000L));
+        Assert.assertThat(date_2.getTime(), is(3L));
     }
 
     public static class LongNumber extends Event{
@@ -129,9 +140,10 @@ public class Push_2Test extends StreamInprocessTest {
 
     }
 
-    public static class UpdateCount {
+    public static class UpdateCount extends Number{
 
         public int count;
+
 
         public int getCount() {
             return count;
@@ -139,6 +151,26 @@ public class Push_2Test extends StreamInprocessTest {
 
         public void setCount(int count) {
             this.count = count;
+        }
+        
+        @Override
+        public double doubleValue() {
+            return getCount();
+        }
+
+        @Override
+        public float floatValue() {
+            return getCount();
+        }
+
+        @Override
+        public int intValue() {
+            return getCount();
+        }
+
+        @Override
+        public long longValue() {
+            return getCount();
         }
 
         @OnEvent
@@ -152,14 +184,24 @@ public class Push_2Test extends StreamInprocessTest {
 
         public int count;
         public int val;
+        public int updatePushVal;
+        public long longVal;
 
         @OnEvent
         public void update() {
             count++;
         }
+        
+        public void pushUpdateCount(UpdateCount update){
+            this.updatePushVal = update.getCount() * 100;
+        }
 
         public void setVal(int val) {
             this.val = val * 10;
+        }
+        
+        public void setLongCount(long longVal){
+            this.longVal = longVal * 1000;
         }
 
         public int getCount() {
