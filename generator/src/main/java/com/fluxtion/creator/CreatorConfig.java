7898c685663c390302382c7241261f95ac9b43cc
Allsimon <simon.allegraud@gmail.com>
2019-06-26 00:23:09 +0200

    refactor(code): use lombok to reduce boilerplate

--- generator/src/main/java/com/fluxtion/creator/CreatorConfig.java
+++ generator/src/main/java/com/fluxtion/creator/CreatorConfig.java
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright (c) 2019, V12 Technology Ltd.
  * All rights reserved.
  *
@@ -12,20 +12,24 @@
  * Server Side Public License for more details.
  *
  * You should have received a copy of the Server Side Public License
- * along with this program.  If not, see 
+ * along with this program.  If not, see
  * <http://www.mongodb.com/licensing/server-side-public-license>.
  */
 package com.fluxtion.creator;
 
 import com.fluxtion.builder.generation.GenerationContext;
-import java.util.ArrayList;
-import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.ClassUtils;
+
+import java.util.List;
 
 /**
  *
  * @author gregp
  */
+@Data
+@NoArgsConstructor
 public class CreatorConfig {
 
     private String outputSepConfigClass;
@@ -35,63 +39,12 @@ public class CreatorConfig {
     private List<EventDefinition> events;
     private String processorId;
 
-    public CreatorConfig(String processorId, String sepConfigClass, String packageName) {
-        this.processorId = processorId;
-        this.outputSepConfigClass = sepConfigClass;
-        this.outputPackage = packageName;
-        nodes = new ArrayList<>();
-        events = new ArrayList<>();
-    }
-
-    public CreatorConfig() {
-    }
-
-    public String getOutputSepConfigClass() {
-        return outputSepConfigClass;
-    }
-
-    public void setOutputSepConfigClass(String outputSepConfigClass) {
-        this.outputSepConfigClass = outputSepConfigClass;
-    }
-
     public String getSepCfgShortClassName() {
         return ClassUtils.getShortCanonicalName(outputSepConfigClass);
     }
 
     public String getSepCfgPackageName() {
         return ClassUtils.getPackageCanonicalName(outputSepConfigClass);
-    }
-
-    public String getOutputPackage() {
-        return outputPackage;
-    }
-
-    public void setOutputPackage(String outputPackage) {
-        this.outputPackage = outputPackage;
-    }
-
-    public List<Node> getNodes() {
-        return nodes;
-    }
-
-    public void setNodes(List<Node> nodes) {
-        this.nodes = nodes;
-    }
-
-    public String getProcessorId() {
-        return processorId;
-    }
-
-    public void setProcessorId(String processorId) {
-        this.processorId = processorId;
-    }
-
-    public String getAuditorClass() {
-        return auditorClass;
-    }
-
-    public void setAuditorClass(String auditorClass) {
-        this.auditorClass = auditorClass;
     }
 
     public void addNode(Node node) {
@@ -116,17 +69,8 @@ public class CreatorConfig {
         return node;
     }
 
-    public List<EventDefinition> getEvents() {
-        return events;
-    }
-
-    public void setEvents(List<EventDefinition> events) {
-        this.events = events;
-    }
-
     public void validateConfig() {
         nodes.stream().forEach(this::cleanNode);
         events.stream().forEach(this::cleanEvent);
     }
-
 }
