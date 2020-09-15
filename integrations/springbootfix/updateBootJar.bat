91db2c6b26f342a7c4b980398f80cd7687eac6b4
greg higgins <greg.higgins@v12technology.com>
2020-09-15 23:40:22 +0100

    added disruptor-3.4.2.jar to patching use java_home in patching script

--- integrations/springbootfix/updateBootJar.bat
+++ integrations/springbootfix/updateBootJar.bat
@@ -3,8 +3,8 @@
 @title Create folder with batch but only if it doesn't already exist - D3F4ULT
 @color 0a
 @cls
-set jar="C:\Program Files\Java\jdk-14\bin\jar.exe"
-set java="C:\Program Files\Java\jdk-14\bin\java.exe"
+set jar="%java_home%\bin\jar.exe"
+set java="%java_home%\bin\java.exe"
 
 Echo Launch dir: "%~dp0"
 Echo Current dir: "%CD%"
@@ -17,10 +17,13 @@ if not exist %~dp0extract (
     %jar% -xvf ..\fluxtion-streaming-api-2.6.3-SNAPSHOT.jar
     %jar% -xvf ..\fluxtion-text-api-2.6.3-SNAPSHOT.jar
     %jar% -xvf ..\lombok-1.18.12.jar
+    %jar% -xvf ..\disruptor-3.4.2.jar
     cd "%CD%"  
 ) 
 
-echo parching classes
+echo patching lombok classes
 %jar% uf %~dp0../target/fluxtion-integration-2.6.3-SNAPSHOT.jar  -C %extractDir% lombok
 timeout /t 2
+echo patching fluxtion classes
 %jar% uf %~dp0../target/fluxtion-integration-2.6.3-SNAPSHOT.jar  -C %extractDir% com
+echo completed parching 
