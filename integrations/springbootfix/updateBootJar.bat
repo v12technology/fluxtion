950078c512e8ee5c44dbd044f628ffd868668b9c
greg higgins <greg.higgins@v12technology.com>
2020-10-30 10:59:58 +0000

    removed hardcoded links to fluxtion jars in patch script, use pom version and relative paths of repos removed lombok jar

--- integrations/springbootfix/updateBootJar.bat
+++ integrations/springbootfix/updateBootJar.bat
@@ -5,21 +5,24 @@
 @cls
 set jar="%java_home%\bin\jar.exe"
 set java="%java_home%\bin\java.exe"
+set version=%1
 
-Echo Launch dir: "%~dp0"
-Echo Current dir: "%CD%"
+Echo Launch dir  : "%~dp0"
+Echo Current dir : "%CD%"
+echo version     : "%version%"
+
 set extractDir=%~dp0extract\
 if not exist %~dp0extract (
     echo Folder does not exist %extractDir% extracting classes to patch
     mkdir "%extractDir%"
     cd %extractDir%
-    %jar% -xvf ..\fluxtion-api-2.6.3-SNAPSHOT.jar
-    %jar% -xvf ..\fluxtion-streaming-api-2.6.3-SNAPSHOT.jar
-    %jar% -xvf ..\fluxtion-text-api-2.6.3-SNAPSHOT.jar
+    %jar% -xvf ..\..\..\api\target\fluxtion-api-%version%.jar
+    %jar% -xvf ..\..\..\extensions\streaming\api\target\fluxtion-streaming-api-%version%.jar
+    %jar% -xvf ..\..\..\extensions\text\api\target\fluxtion-text-api-%version%.jar
     %jar% -xvf ..\disruptor-3.4.2.jar
     cd "%CD%"  
 ) 
 
 echo patching fluxtion classes
-%jar% uf %~dp0../target/fluxtion-integration-2.6.3-SNAPSHOT.jar  -C %extractDir% com
-echo completed parching 
+%jar% uf %~dp0../target/fluxtion-integration-%version%.jar  -C %extractDir% com
+echo completed patching 
--- integrations/springbootfix/updateBootJar.sh
+++ integrations/springbootfix/updateBootJar.sh
@@ -4,14 +4,16 @@ set -m
 echo "patching springboot fat jar with fluxtion classes"
 jar=$JAVA_HOME/bin/jar
 java=$JAVA_HOME/bin/java
+version=$1
 
 CD=`pwd`
 cd `dirname $0`
 SCRIPTDIR=`pwd`
 extractDir=$SCRIPTDIR/extract/
 
-echo Launch dir: $SCRIPTDIR
-echo Current dir: $CD
+echo Launch dir  : $SCRIPTDIR
+echo Current dir : $CD
+echo version     : $version
 echo extractDir $extractDir
 
 if [ ! -d $extractDir ] 
@@ -19,15 +21,15 @@ then
     echo "Folder does not exist $extractDir extracting classes to patch"
     mkdir $extractDir
     cd $extractDir
-    $jar -xvf ../fluxtion-api-2.6.3-SNAPSHOT.jar
-    $jar -xvf ../fluxtion-streaming-api-2.6.3-SNAPSHOT.jar
-    $jar -xvf ../fluxtion-text-api-2.6.3-SNAPSHOT.jar
+    $jar -xvf ../../../api/target/fluxtion-api-%version%.jar
+    $jar -xvf ../../../extensions/streaming/api/target/fluxtion-streaming-api-%version%.jar
+    $jar -xvf ../../../extensions/text/api/target/fluxtion-text-api-%version%.jar
     $jar -xvf ../disruptor-3.4.2.jar  
 fi
 cd $CD
 
 echo "patching fluxtion classes"
-$jar uf $SCRIPTDIR/../target/fluxtion-integration-2.6.3-SNAPSHOT.jar  -C $extractDir com
+$jar uf $SCRIPTDIR/../target/fluxtion-integration-$version.jar  -C $extractDir com
 echo "completed patching"
 
 
