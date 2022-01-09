f93c35843d5b5cbd315fcdcf16f35096b3db4800
greg higgins <greg.higgins@v12technology.com>
2022-01-09 22:23:34 +0000

    Remove java 15 from build matrix code tidy

--- compiler/src/main/java/com/fluxtion/compiler/generation/compiler/classcompiler/StringCompilation.java
+++ compiler/src/main/java/com/fluxtion/compiler/generation/compiler/classcompiler/StringCompilation.java
@@ -5,23 +5,25 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Arrays;
 
-public class StringCompilation {
+public interface StringCompilation {
+
 
     /**
-     * compile a string to a java class
-     * @param className fully qualified class name
-     * @throws Exception throws {@link RuntimeException} if fails to compile
+     *
+     * @param source java source to compile
+     * @param <T> The compiled class type
+     * @return Compiled class
+     * @throws URISyntaxException if fails to compile
+     * @throws IOException if fails to compile
+     * @throws ClassNotFoundException if fails to compile
      */
     @SuppressWarnings({"unchecked"})
-    public static<T>  Class<T> compile(String className, String source) throws URISyntaxException, IOException, ClassNotFoundException {
+    static<T>  Class<T> compile(String className, String source) throws URISyntaxException, IOException, ClassNotFoundException {
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
         DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
         final JavaByteObject byteObject = new JavaByteObject(className);
         StandardJavaFileManager standardFileManager = compiler.getStandardFileManager(diagnostics, null, null);
         JavaFileManager fileManager = createFileManager(standardFileManager, byteObject);
-//        List<String> optionList = new ArrayList<>();
-//        optionList.add("-classpath");
-//        optionList.add(System.getProperty("java.class.path"));
         JavaCompiler.CompilationTask task = compiler.getTask(
                 null, fileManager, diagnostics, null, null, Arrays.asList(new JavaStringObject(className, source))
         );
@@ -31,15 +33,11 @@ public class StringCompilation {
             throw new RuntimeException("unable to compile source file to class:'" + className + "'");
         }
         fileManager.close();
-
-        //loading and using our compiled class
         final ClassLoader inMemoryClassLoader = createClassLoader(byteObject);
         return (Class<T>) inMemoryClassLoader.loadClass(className);
-//        EventProcessor processor = test.getDeclaredConstructor().newInstance();
-//        processor.onEvent("hello world");
     }
 
-    private static JavaFileManager createFileManager(StandardJavaFileManager fileManager, JavaByteObject byteObject) {
+    static JavaFileManager createFileManager(StandardJavaFileManager fileManager, JavaByteObject byteObject) {
         return new ForwardingJavaFileManager(fileManager) {
             @Override
             public JavaFileObject getJavaFileForOutput(Location location,
@@ -50,11 +48,10 @@ public class StringCompilation {
         };
     }
 
-    private static ClassLoader createClassLoader(final JavaByteObject byteObject) {
+    static ClassLoader createClassLoader(final JavaByteObject byteObject) {
         return new ClassLoader() {
             @Override
             public Class<?> findClass(String name) throws ClassNotFoundException {
-                //no need to search class path, we already have byte code.
                 byte[] bytes = byteObject.getBytes();
                 return defineClass(name, bytes, 0, bytes.length);
             }
