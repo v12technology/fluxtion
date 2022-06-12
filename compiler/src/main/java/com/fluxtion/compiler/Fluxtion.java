aff4c8718f030b905a3db07ce510bd5ecdfec383
greg higgins <greg.higgins@v12technology.com>
2022-06-12 22:17:26 +0100

    support aot compilation for root nodes and builders. Added some mappers and predicates for primitives.

--- compiler/src/main/java/com/fluxtion/compiler/Fluxtion.java
+++ compiler/src/main/java/com/fluxtion/compiler/Fluxtion.java
@@ -15,42 +15,56 @@ public interface Fluxtion {
 
     /**
      * Generates and compiles Java source code for a {@link StaticEventProcessor}. The compiled version only requires
-     * the Fluxtion runtime dependencies to operate and process events.
+     * the Fluxtion runtime dependencies to operate and process events. The source code is only maintained in memory
+     * as a string and is not persisted,
      *
+     * <p>
      * {@link Lifecycle#init()} has not been called on the returned instance. The caller must invoke init before
      * sending events to the processor using {@link StaticEventProcessor#onEvent(Object)}
      *
-     * @see EventProcessorConfig
      * @param sepConfig the configuration used to build this {@link StaticEventProcessor}
      * @return An uninitialized instance of a {@link StaticEventProcessor}
+     * @see EventProcessorConfig
      */
     @SneakyThrows
-    static EventProcessor compile(SerializableConsumer<EventProcessorConfig> sepConfig){
-       return (EventProcessor) EventProcessorFactory.compile(sepConfig);
+    static EventProcessor compile(SerializableConsumer<EventProcessorConfig> sepConfig) {
+        return EventProcessorFactory.compile(sepConfig);
+    }
+
+    @SneakyThrows
+    static EventProcessor compile(SerializableConsumer<EventProcessorConfig> sepConfig,
+                                  SerializableConsumer<FluxtionCompilerConfig> cfgBuilder) {
+        return EventProcessorFactory.compile(sepConfig, cfgBuilder);
+    }
+
+    @SneakyThrows
+    static EventProcessor compileAot(SerializableConsumer<EventProcessorConfig> cfgBuilder) {
+        String packageName = (cfgBuilder.getContainingClass().getCanonicalName() + "." + cfgBuilder.method().getName()).toLowerCase();
+        return compile(cfgBuilder, compilerCfg -> compilerCfg.setPackageName(packageName));
     }
 
     /**
      * Generates an in memory version of a {@link StaticEventProcessor}. The in memory version is transient and requires
      * the runtime and compiler Fluxtion libraries to operate.
-     *
+     * <p>
      * {@link Lifecycle#init()} has not been called on the returned instance. The caller must invoke init before
      * sending events to the processor using {@link StaticEventProcessor#onEvent(Object)}
      *
-     * @see EventProcessorConfig
      * @param sepConfig the configuration used to build this {@link StaticEventProcessor}
      * @return An uninitialized instance of a {@link StaticEventProcessor}
+     * @see EventProcessorConfig
      */
-    static EventProcessor interpret(SerializableConsumer<EventProcessorConfig> sepConfig){
+    static EventProcessor interpret(SerializableConsumer<EventProcessorConfig> sepConfig) {
         return EventProcessorFactory.interpreted(sepConfig);
     }
 
     /**
      * Generates and compiles Java source code for a {@link StaticEventProcessor}. The compiled version only requires
      * the Fluxtion runtime dependencies to operate and process events.
-     *
+     * <p>
      * {@link Lifecycle#init()} has not been called on the returned instance. The caller must invoke init before
      * sending events to the processor using {@link StaticEventProcessor#onEvent(Object)}
-     *
+     * <p>
      * The root node is injected into the graph. If the node has any injected dependencies these are added to the
      * graph. If a custom builder for the root node exists this will called and additional nodes can be added to the
      * graph in the factory method.
@@ -59,17 +73,35 @@ public interface Fluxtion {
      * @return An uninitialized instance of a {@link StaticEventProcessor}
      */
     @SneakyThrows
-    static EventProcessor compile(RootInjectedNode rootNode){
-        return (EventProcessor) EventProcessorFactory.compile(rootNode);
+    static EventProcessor compile(RootInjectedNode rootNode) {
+        return EventProcessorFactory.compile(rootNode);
     }
+
+    @SneakyThrows
+    static EventProcessor compile(RootInjectedNode rootNode, SerializableConsumer<FluxtionCompilerConfig> cfgBuilder) {
+        return EventProcessorFactory.compile(rootNode, cfgBuilder);
+    }
+
+    @SneakyThrows
+    static EventProcessor compileAot(RootInjectedNode rootNode) {
+        String pkg = (rootNode.getRootClass().getCanonicalName() + "." + rootNode.getName()).toLowerCase();
+        return EventProcessorFactory.compile(rootNode, compilerCfg -> compilerCfg.setPackageName(pkg));
+    }
+
+    @SneakyThrows
+    static EventProcessor compileAot(RootInjectedNode rootNode, String packagePrefix) {
+        String pkg = (packagePrefix + "." + rootNode.getName()).toLowerCase();
+        return EventProcessorFactory.compile(rootNode, compilerCfg -> compilerCfg.setPackageName(pkg));
+    }
+
 
     /**
      * Generates an in memory version of a {@link StaticEventProcessor}. The in memory version is transient and requires
      * the runtime and compiler Fluxtion libraries to operate.
-     *
+     * <p>
      * {@link Lifecycle#init()} has not been called on the returned instance. The caller must invoke init before
      * sending events to the processor using {@link StaticEventProcessor#onEvent(Object)}
-     *
+     * <p>
      * The root node is injected into the graph. If the node has any injected dependencies these are added to the
      * graph. If a custom builder for the root node exists this will called and additional nodes can be added to the
      * graph in the factory method.
@@ -78,7 +110,7 @@ public interface Fluxtion {
      * @return An uninitialized instance of a {@link StaticEventProcessor}
      */
     @SneakyThrows
-    static EventProcessor interpret(RootInjectedNode rootNode){
+    static EventProcessor interpret(RootInjectedNode rootNode) {
         return EventProcessorFactory.interpreted(rootNode);
     }
 }
--- compiler/src/main/java/com/fluxtion/compiler/generation/EventProcessorFactory.java
+++ compiler/src/main/java/com/fluxtion/compiler/generation/EventProcessorFactory.java
@@ -23,17 +23,13 @@ import com.fluxtion.compiler.builder.factory.RootInjectedNode;
 import com.fluxtion.compiler.generation.compiler.EventProcessorCompilation;
 import com.fluxtion.compiler.generation.compiler.EventProcessorGenerator;
 import com.fluxtion.compiler.generation.targets.InMemoryEventProcessor;
-import com.fluxtion.runtime.StaticEventProcessor;
-import com.fluxtion.runtime.lifecycle.Lifecycle;
-import com.fluxtion.runtime.partition.LambdaReflection.SerializableBiConsumer;
+import com.fluxtion.runtime.EventProcessor;
 import com.fluxtion.runtime.partition.LambdaReflection.SerializableConsumer;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.util.function.Consumer;
-
-import static com.fluxtion.compiler.generation.GenerationContext.SINGLETON;
 
 /**
  * Generates and compiles a SEP for use by a caller in the same process. The compilation is invoked programmatically
@@ -77,21 +73,26 @@ public class EventProcessorFactory {
     }
 
 
-
-    public static StaticEventProcessor compile(SerializableConsumer<EventProcessorConfig> builder) throws Exception {
+    public static EventProcessor compile(SerializableConsumer<EventProcessorConfig> builder) throws Exception {
         String name = "Processor";
         String pkg = (builder.getContainingClass().getCanonicalName() + "." + builder.method().getName()).toLowerCase();
-        return (compile(name, pkg, builder));
+        return compile(name, pkg, builder);
     }
 
-    public static StaticEventProcessor compile(RootInjectedNode rootNode) throws Exception {
+    public static EventProcessor compile(RootInjectedNode rootNode) throws Exception {
         SerializableConsumer<EventProcessorConfig> builder = (EventProcessorConfig cfg) -> cfg.setRootInjectedNode(rootNode);
         String name = "Processor";
-        String pkg = (builder.getContainingClass().getCanonicalName() + "." + builder.method().getName()).toLowerCase();
-        return EventProcessorFactory.compile(pkg, name, builder);
+        String pkg = (rootNode.getClass().getCanonicalName() + "." + rootNode.getName()).toLowerCase();
+        return compile(pkg, name, builder);
     }
 
-    public static StaticEventProcessor compileTestInstance(Consumer<EventProcessorConfig> cfgBuilder,
+    @SneakyThrows
+    public static EventProcessor compile(RootInjectedNode rootNode, SerializableConsumer<FluxtionCompilerConfig> cfgBuilder) {
+        SerializableConsumer<EventProcessorConfig> builder = (EventProcessorConfig cfg) -> cfg.setRootInjectedNode(rootNode);
+        return compile(builder, cfgBuilder);
+    }
+
+    public static EventProcessor compileTestInstance(Consumer<EventProcessorConfig> cfgBuilder,
                                                            String pckg,
                                                            String sepName,
                                                            boolean writeSourceFile,
@@ -111,14 +112,13 @@ public class EventProcessorFactory {
      * Build a static event processor using the supplied consumer to populate the SEPConfig. Will always build a new
      * processor, supplying a newly created instance of the class to the caller.
      *
-     *
      * @param name    The name of the generated static event processor
      * @param pkg     The package name of the generated static event processor
      * @param builder The Consumer that populates the SEPConfig
      * @return An instance of the newly generated static event processor
      * @throws Exception
      */
-    public static StaticEventProcessor compile(String name, String pkg, Consumer<EventProcessorConfig> builder) throws Exception {
+    public static EventProcessor compile(String name, String pkg, Consumer<EventProcessorConfig> builder) throws Exception {
         return compile(
                 builder,
                 pkg,
@@ -126,26 +126,26 @@ public class EventProcessorFactory {
                 OutputRegistry.JAVA_SRC_DIR,
                 OutputRegistry.RESOURCE_DIR,
                 true,
-                true,
-                true);
+                false,
+                false);
     }
 
     /**
      * Compiles and instantiates a SEP described with the provided {@link EventProcessorConfig}, optionally initialising the SEP
      * instance.
      *
-     * @param cfgBuilder - A client consumer to buld sep using the provided
-     * @param packageName       - output package of the generated class
-     * @param className    - output class name of the generated SEP
-     * @param srcGenDir  - output directory for generated SEP source files
-     * @param resGenDir  - output directory for generated resources
-     * @param initialise - if true call init method on SEP instance
+     * @param cfgBuilder  - A client consumer to buld sep using the provided
+     * @param packageName - output package of the generated class
+     * @param className   - output class name of the generated SEP
+     * @param srcGenDir   - output directory for generated SEP source files
+     * @param resGenDir   - output directory for generated resources
+     * @param initialise  - if true call init method on SEP instance
      * @return
      * @throws InstantiationException
      * @throws IllegalAccessException
      * @throws Exception
      */
-    public static StaticEventProcessor compile(Consumer<EventProcessorConfig> cfgBuilder,
+    public static EventProcessor compile(Consumer<EventProcessorConfig> cfgBuilder,
                                                String packageName,
                                                String className,
                                                String srcGenDir,
@@ -155,25 +155,24 @@ public class EventProcessorFactory {
                                                boolean generateMetaInformation) throws InstantiationException, IllegalAccessException, Exception {
         EventProcessorCompilation compiler = new EventProcessorCompilation();
         FluxtionCompilerConfig compilerCfg = new FluxtionCompilerConfig();
-        compilerCfg.setOutputDirectory(SINGLETON.getSourceRootDirectory().getCanonicalPath());
-        compilerCfg.setResourcesOutputDirectory(SINGLETON.getResourcesRootDirectory().getCanonicalPath());
+        compilerCfg.setOutputDirectory(new File(srcGenDir).getCanonicalPath());
+        compilerCfg.setResourcesOutputDirectory(new File(resGenDir).getCanonicalPath());
         compilerCfg.setPackageName(packageName);
         compilerCfg.setClassName(className);
         compilerCfg.setWriteSourceToFile(writeSourceFile);
         compilerCfg.setGenerateDescription(generateMetaInformation);
 
-        Class<StaticEventProcessor> sepClass = compiler.compile(compilerCfg, new InProcessEventProcessorConfig(cfgBuilder));
-        StaticEventProcessor sep = sepClass.getDeclaredConstructor().newInstance();
+        Class<EventProcessor> sepClass = compiler.compile(compilerCfg, new InProcessEventProcessorConfig(cfgBuilder));
+        EventProcessor sep = sepClass.getDeclaredConstructor().newInstance();
         if (initialise) {
-            if (sep instanceof Lifecycle) {
-                ((Lifecycle) sep).init();
-            }
+            sep.init();
         }
         return sep;
     }
 
-    public static StaticEventProcessor compile(
-            SerializableBiConsumer<EventProcessorConfig,  FluxtionCompilerConfig> cfgBuilder)
+    public static EventProcessor compile(
+            SerializableConsumer<EventProcessorConfig> sepConfig,
+            SerializableConsumer<FluxtionCompilerConfig> cfgBuilder)
             throws Exception {
         String className = "Processor";
         String packageName = (cfgBuilder.getContainingClass().getCanonicalName() + "." + cfgBuilder.method().getName()).toLowerCase();
@@ -184,15 +183,12 @@ public class EventProcessorFactory {
         fluxtionCompilerConfig.setClassName(className);
         fluxtionCompilerConfig.setWriteSourceToFile(true);
         fluxtionCompilerConfig.setGenerateDescription(true);
-        EventProcessorConfig eventProcessorConfig = new EventProcessorConfig();
 
-        cfgBuilder.accept(eventProcessorConfig, fluxtionCompilerConfig);
+        cfgBuilder.accept(fluxtionCompilerConfig);
         EventProcessorCompilation compiler = new EventProcessorCompilation();
-        Class<StaticEventProcessor> sepClass = compiler.compile(fluxtionCompilerConfig, eventProcessorConfig);
-        StaticEventProcessor sep = sepClass.getDeclaredConstructor().newInstance();
-        if (sep instanceof Lifecycle) {
-            ((Lifecycle) sep).init();
-        }
+        Class<EventProcessor> sepClass = compiler.compile(fluxtionCompilerConfig, new InProcessEventProcessorConfig(sepConfig));
+        EventProcessor sep = sepClass.getDeclaredConstructor().newInstance();
+        sep.init();
         return sep;
     }
 
