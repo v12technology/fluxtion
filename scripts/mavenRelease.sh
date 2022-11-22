777d438217502b3603a75c57fc77de9f3197753a
greg <gregphiggins@gmai.com>
2022-11-22 15:04:41 +0000

    added release script

---
+++ scripts/mavenRelease.sh
@@ -0,0 +1,2 @@
+echo releasing fluxtion
+mvn --batch-mode external.atlassian.jgitflow:jgitflow-maven-plugin:1.0-m5.1:release-finish -Prelease -Dmaven.javadoc.failOnError=false -DpushReleases=false -Dfluxtion.github.user=${{ secrets.FLUXTION_GITHUB_USER }} -Dfluxtion.github.password=${{ secrets.FLUXTION_GITHUB_TOKEN }} &&
