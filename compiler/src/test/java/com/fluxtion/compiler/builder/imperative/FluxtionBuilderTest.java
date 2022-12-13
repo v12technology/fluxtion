fd87c4bd3c1d34ce70d314a30dc3344281f2b8a1
greg higgins <greg.higgins@v12technology.com>
2022-12-13 12:08:29 +0000

    Release 7.0.1 (#171)
    optionally write generated source to  supplied writer, now in Fluxtion#compile
    Added annotation processors to validate the Fluxtion annotations in client code are valid.
    
    
    Co-authored-by: command-line <gregphiggins@gmail.com>

--- compiler/src/test/java/com/fluxtion/compiler/builder/imperative/FluxtionBuilderTest.java
+++ compiler/src/test/java/com/fluxtion/compiler/builder/imperative/FluxtionBuilderTest.java
@@ -8,11 +8,14 @@ import com.fluxtion.compiler.generation.compiler.classcompiler.StringCompilation
 import com.fluxtion.runtime.EventProcessor;
 import com.fluxtion.runtime.annotations.OnEventHandler;
 import com.fluxtion.runtime.annotations.builder.Disabled;
+import lombok.SneakyThrows;
 import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
@@ -27,6 +30,48 @@ public class FluxtionBuilderTest {
     public static final String PACKAGE_DIR = OUTPUT_DIRECTORY + "/" + PACKAGE_NAME.replace(".", "/");
     public static final String PROCESSOR = "MyProcessor";
 
+
+    @Test
+    @SneakyThrows
+    public void generateToStringWriterTest() {
+        StringWriter writer = new StringWriter();
+        Fluxtion.compile(c -> c.addNode(new MyStringHandler()), writer);
+        Assert.assertFalse(writer.toString().isEmpty());
+    }
+
+    @SneakyThrows
+    @Test(expected = RuntimeException.class)
+    public void failCompileString() {
+        String source = "    " +
+                "import com.fluxtion.runtime.annotations.OnEventHandler;\n" +
+                "\n" +
+                "public class MyStringHandler {\n" +
+                "    String in;\n" +
+                "\n" +
+                "    @OnEventHandler\n" +
+                "    void stringUpdated() {\n" +
+                "        this.in = in;\n" +
+                "    }\n" +
+                "}";
+
+        StringCompilation.compile("MyStringHandler", source);
+    }
+
+    @Test
+    @SneakyThrows
+    public void generateToStringWriterAndCompileTest() {
+        StringWriter writer = new StringWriter();
+        Fluxtion.compile(c -> c.addNode(new MyStringHandler(), "handler"), c -> {
+            c.setSourceWriter(writer);
+            c.setPackageName("com.whatever");
+            c.setClassName("MYProcessor");
+        });
+        Class<EventProcessor> sepClass = StringCompilation.compile("com.whatever.MYProcessor", writer.toString());
+        EventProcessor processor = sepClass.getDeclaredConstructor().newInstance();
+        processor.init();
+        processor.onEvent("HELLO");
+        assertThat(processor.<MyStringHandler>getNodeById("handler").in, is("HELLO"));
+    }
 
     @Test
     public void buildFromFluxtionGraphBuilder() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException, IOException, URISyntaxException, NoSuchFieldException {
