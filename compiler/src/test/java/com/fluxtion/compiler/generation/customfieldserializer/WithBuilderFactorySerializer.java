6c5ad98c868c915e88dd5b09c259ab4bbc5689c0
greg higgins <greg.higgins@v12technology.com>
2023-03-22 18:59:50 +0000

    support more standard serializers for time, file, url and formatting (#223)
    Co-authored-by: command-line <gregphiggins@gmail.com>

--- compiler/src/test/java/com/fluxtion/compiler/generation/customfieldserializer/WithBuilderFactorySerializer.java
+++ compiler/src/test/java/com/fluxtion/compiler/generation/customfieldserializer/WithBuilderFactorySerializer.java
@@ -6,14 +6,14 @@ import com.fluxtion.compiler.generation.serialiser.FieldToSourceSerializer;
 import com.google.auto.service.AutoService;
 
 @AutoService(FieldToSourceSerializer.class)
-public class WithBuilderFactorySerializer implements FieldToSourceSerializer {
+public class WithBuilderFactorySerializer implements FieldToSourceSerializer<WithBuilderFactory> {
     @Override
     public boolean typeSupported(Class<?> type) {
         return WithBuilderFactory.class.isAssignableFrom(type);
     }
 
     @Override
-    public String mapToSource(FieldContext fieldContext) {
+    public String mapToSource(FieldContext<WithBuilderFactory> fieldContext) {
         fieldContext.getImportList().add(WithBuilderFactory.class);
         WithBuilderFactory withBuilder = (WithBuilderFactory) fieldContext.getInstanceToMap();
         return "WithBuilderFactory.createWithBuilder(\"" + withBuilder.getId() + "\")";
