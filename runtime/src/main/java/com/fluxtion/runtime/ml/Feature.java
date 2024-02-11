193b61923bf75486501ad1126d15529d3e77e00a
command-line <gregphiggins@gmail.com>
2024-02-11 13:18:32 +0000

    supports using lambdas to include features from a flowsupplier without having to manually create features

--- runtime/src/main/java/com/fluxtion/runtime/ml/Feature.java
+++ runtime/src/main/java/com/fluxtion/runtime/ml/Feature.java
@@ -2,9 +2,13 @@ package com.fluxtion.runtime.ml;
 
 import com.fluxtion.runtime.annotations.ExportService;
 import com.fluxtion.runtime.annotations.feature.Experimental;
+import com.fluxtion.runtime.dataflow.FlowSupplier;
 import com.fluxtion.runtime.node.NamedNode;
+import com.fluxtion.runtime.partition.LambdaReflection;
 
 import java.beans.Introspector;
+import java.util.ArrayList;
+import java.util.List;
 
 @Experimental
 public interface Feature extends NamedNode, @ExportService CalibrationProcessor {
@@ -19,5 +23,16 @@ public interface Feature extends NamedNode, @ExportService CalibrationProcessor 
     }
 
     double value();
+
+    @SafeVarargs
+    static <T> List<Feature> include(FlowSupplier<T> inputDataFlow, LambdaReflection.SerializableToDoubleFunction<T>... featureExtractors) {
+        List<Feature> featureList = new ArrayList<>(featureExtractors.length);
+        for (LambdaReflection.SerializableToDoubleFunction<T> featureExtractor : featureExtractors) {
+            featureList.add(
+                    PropertyToFeature.build(featureExtractor.method().getName(), inputDataFlow, featureExtractor)
+            );
+        }
+        return featureList;
+    }
 
 }
