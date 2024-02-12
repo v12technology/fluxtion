1ca997cc9f62d8c5b7d679e41e506fa5410589dc
greg <greg.h@vidarrcap.com>
2024-02-12 00:39:22 +0000

    override directories with scan

--- compiler/src/main/java/com/fluxtion/compiler/generation/RuntimeConstants.java
+++ compiler/src/main/java/com/fluxtion/compiler/generation/RuntimeConstants.java
@@ -3,4 +3,6 @@ package com.fluxtion.compiler.generation;
 public interface RuntimeConstants {
     String FLUXTION_NO_COMPILE = "FLUXTION.NO_COMPILE";
     String GENERATION_CLASSPATH = "FLUXTION.GENERATION.CLASSPATH";
+    String OUTPUT_DIRECTORY = "FLUXTION.OUTPUT.DIRECTORY";
+    String RESOURCES_DIRECTORY = "FLUXTION.RESOURCES.DIRECTORY";
 }
