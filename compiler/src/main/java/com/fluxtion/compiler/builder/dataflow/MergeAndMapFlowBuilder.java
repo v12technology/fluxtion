1fd6d78f197b93f03cba33e48b992fba4a011059
greg higgins <greg.higgins@v12technology.com>
2024-05-13 21:07:10 +0100

    release 9.3.6 (#293)
    * updating poms for 9.3.6-SNAPSHOT development
    
    * updating poms for branch'release/9.3.5' with non-snapshot versions
    
    * updating develop poms to master versions to avoid merge conflicts
    
    * Updating develop poms back to pre merge state
    
    * remove co-group
    
    * add dataFlow() method to MergeAndMapFlowBuilder
    
    ---------
    
    Co-authored-by: runner <runner@fv-az1427-595.ftqghr0h53yelcozmorifnlbzb.ex.internal.cloudapp.net>
    Co-authored-by: greg <greg.h@vidarrcap.com>

--- compiler/src/main/java/com/fluxtion/compiler/builder/dataflow/MergeAndMapFlowBuilder.java
+++ compiler/src/main/java/com/fluxtion/compiler/builder/dataflow/MergeAndMapFlowBuilder.java
@@ -1,5 +1,6 @@
 package com.fluxtion.compiler.builder.dataflow;
 
+import com.fluxtion.runtime.dataflow.FlowFunction;
 import com.fluxtion.runtime.dataflow.function.MergeMapFlowFunction;
 import com.fluxtion.runtime.dataflow.function.MergeProperty;
 import com.fluxtion.runtime.partition.LambdaReflection;
@@ -7,6 +8,11 @@ import com.fluxtion.runtime.partition.LambdaReflection;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A builder that merges and maps several {@link FlowFunction}'s into a single event stream of type T
+ *
+ * @param <T> The output type of the merged stream
+ */
 public class MergeAndMapFlowBuilder<T> {
 
     private final LambdaReflection.SerializableSupplier<T> resultFactory;
@@ -36,4 +42,12 @@ public class MergeAndMapFlowBuilder<T> {
         return stream;
     }
 
+    /**
+     * Merges and maps several {@link FlowFunction}'s into a single event stream of type T
+     *
+     * @return An {@link FlowBuilder} that can used to construct stream processing logic
+     */
+    public FlowBuilder<T> dataFlow() {
+        return new FlowBuilder<>(build());
+    }
 }
