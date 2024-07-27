50c3413e9080ae8f395e3f66f1b168dd8211bf3e
greg h <gregphiggins@gmail.com>
2024-07-27 08:49:34 +0100

    remove experimental fluxtion server

--- runtime/src/main/java/com/fluxtion/runtime/server/subscription/EventFlowService.java
+++
@@ -1,9 +0,0 @@
-package com.fluxtion.runtime.server.subscription;
-
-import com.fluxtion.runtime.annotations.feature.Experimental;
-
-@Experimental
-public interface EventFlowService {
-
-    void setEventFlowManager(EventFlowManager eventFlowManager, String serviceName);
-}
