8c8620f63c4fe8862b2cf77d3dc2dc22ca983797
greg h <gregphiggins@gmail.com>
2024-09-25 08:37:49 +0100

    some work for pnl article

--- compiler/src/main/java/com/fluxtion/compiler/generation/compiler/classcompiler/StringCompilation.java
+++ compiler/src/main/java/com/fluxtion/compiler/generation/compiler/classcompiler/StringCompilation.java
@@ -2,6 +2,9 @@ package com.fluxtion.compiler.generation.compiler.classcompiler;
 
 import com.fluxtion.compiler.generation.RuntimeConstants;
 import com.fluxtion.compiler.generation.annotationprocessor.*;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 
 import javax.tools.*;
 import java.io.IOException;
@@ -51,6 +54,50 @@ public interface StringCompilation {
                 new ValidateExportFunctionAnnotations(),
                 new ValidateServiceListenerAnnotations()
         ));
+        if (!task.call()) {
+            diagnostics.getDiagnostics().forEach(System.out::println);
+            throw new RuntimeException("unable to compile source file to class:'" + className + "'");
+        }
+        fileManager.close();
+        final ClassLoader inMemoryClassLoader = createClassLoader(byteObject);
+        return (Class<T>) inMemoryClassLoader.loadClass(className);
+    }
+
+
+    static <T> Class<T> compile(String source, List<String> optionList) throws URISyntaxException, IOException, ClassNotFoundException {
+        CompilationUnit cu = StaticJavaParser.parse(source);
+        ClassOrInterfaceDeclaration classDeclaration = cu.findFirst(ClassOrInterfaceDeclaration.class).orElse(null);
+        String className = classDeclaration.getFullyQualifiedName().get();
+
+        List<ClassOrInterfaceDeclaration> declarations = cu.findAll(ClassOrInterfaceDeclaration.class);
+        List<JavaStringObject> javaStringObjects = new ArrayList<>();
+        declarations.forEach(c -> javaStringObjects.add(new JavaStringObject(c.getFullyQualifiedName().get(), source)));
+
+
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
+        final JavaByteObject byteObject = new JavaByteObject(className);
+        StandardJavaFileManager standardFileManager = compiler.getStandardFileManager(diagnostics, null, null);
+        JavaFileManager fileManager = createFileManager(standardFileManager, byteObject);
+        String cp = System.getProperty(RuntimeConstants.GENERATION_CLASSPATH);
+        if (cp != null) {
+            optionList.add("-classpath");
+            optionList.add(cp);
+        }
+
+        JavaCompiler.CompilationTask task = compiler.getTask(
+                null, fileManager, diagnostics, optionList, null, javaStringObjects);
+
+        task.setProcessors(
+                Arrays.asList(
+                        new ValidateEventHandlerAnnotations(),
+                        new ValidateLifecycleAnnotations(),
+                        new ValidateOnTriggerAnnotations(),
+                        new ValidateOnParentUpdateHandlerAnnotations(),
+                        new ValidateExportFunctionAnnotations(),
+                        new ValidateServiceListenerAnnotations()
+                ));
+
         if (!task.call()) {
             diagnostics.getDiagnostics().forEach(System.out::println);
             throw new RuntimeException("unable to compile source file to class:'" + className + "'");
