c8096f05418a32ab4ff0b5bc23ab9ae6220c103c
greg higgins <greg.higgins@v12technology.com>
2024-11-13 12:43:00 +0000

    Release 9.3.45 (#318)
    * updating poms for 9.3.45-SNAPSHOT development
    
    * updating poms for branch'release/9.3.44' with non-snapshot versions
    
    * updating develop poms to master versions to avoid merge conflicts
    
    * Updating develop poms back to pre merge state
    
    * add data flow pnl example to docs
    
    * Feature/vararg trigger methods (#317)
    
    * add some helper methods for triggering a data flow
    
    
    
    ---------
    
    Co-authored-by: greg h <gregphiggins@gmail.com>
    
    ---------
    
    Co-authored-by: runner <runner@fv-az568-57.kq21xgauaq5efpa1vyplvqtvth.dx.internal.cloudapp.net>
    Co-authored-by: greg h <greg.higgins@nonco.com>
    Co-authored-by: greg h <gregphiggins@gmail.com>

--- runtime/src/main/java/com/fluxtion/runtime/util/CollectionHelper.java
+++ runtime/src/main/java/com/fluxtion/runtime/util/CollectionHelper.java
@@ -1,8 +1,6 @@
 package com.fluxtion.runtime.util;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public interface CollectionHelper {
     @SafeVarargs
@@ -11,6 +9,20 @@ public interface CollectionHelper {
             return Collections.emptyList();
         } else {
             return Collections.unmodifiableList(Arrays.asList(elements));
+        }
+    }
+
+    @SafeVarargs
+    @SuppressWarnings("varargs")
+    static <K, V> Map<K, V> ofEntries(Map.Entry<? extends K, ? extends V>... entries) {
+        if (entries.length == 0) {
+            return Collections.emptyMap();
+        } else {
+            HashMap<K, V> map = new HashMap<>(entries.length);
+            for (Map.Entry<? extends K, ? extends V> entry : entries) {
+                map.put(entry.getKey(), entry.getValue());
+            }
+            return map;
         }
     }
 }
