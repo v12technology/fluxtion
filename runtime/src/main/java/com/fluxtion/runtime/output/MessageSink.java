e2aace3ac1d1f161384e3738de8acef708d30f7d
greg <gregphiggins@gmail.com>
2024-12-03 14:09:47 +0000

    adds support for value mapping function on MessageSink. AbstractMessageSink applies transform automatically for easier Sink implementation by client code.

--- runtime/src/main/java/com/fluxtion/runtime/output/MessageSink.java
+++ runtime/src/main/java/com/fluxtion/runtime/output/MessageSink.java
@@ -1,12 +1,23 @@
+/*
+ * SPDX-FileCopyrightText: © 2024 Gregory Higgins <greg.higgins@v12technology.com>
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
 package com.fluxtion.runtime.output;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 /**
  * A sink for an EventProcessor. Implement this interface and register with :
- *
+ * <p>
  * {@link com.fluxtion.runtime.EventProcessor#registerService(Object, Class, String)}
+ *
  * @param <T> the type of message published to the Sink
  */
 public interface MessageSink<T> extends Consumer<T> {
+
+    default void setValueMapper(Function<? super T, ?> valueMapper) {
+        throw new UnsupportedOperationException("setValueMapper not implemented");
+    }
 }
