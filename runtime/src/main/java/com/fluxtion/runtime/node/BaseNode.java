e696d047da116b933ce31287de4873eda146df16
greg <gregphiggins@gmail.com>
2025-01-21 10:09:28 +0000

    expose setter for name in BaseNode, NamedBaseNode extends BaseNode and implements NamedNode. Subclasses of NamedBaseNode can now set the name with a property

--- runtime/src/main/java/com/fluxtion/runtime/node/BaseNode.java
+++ runtime/src/main/java/com/fluxtion/runtime/node/BaseNode.java
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: © 2024 Gregory Higgins <greg.higgins@v12technology.com>
+ * SPDX-FileCopyrightText: © 2025 Gregory Higgins <greg.higgins@v12technology.com>
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
@@ -8,14 +8,18 @@ package com.fluxtion.runtime.node;
 import com.fluxtion.runtime.EventProcessorContext;
 import com.fluxtion.runtime.EventProcessorContextListener;
 import com.fluxtion.runtime.annotations.Initialise;
+import com.fluxtion.runtime.annotations.builder.FluxtionIgnore;
 import com.fluxtion.runtime.audit.EventLogNode;
 import lombok.Getter;
+import lombok.Setter;
 
 public class BaseNode extends EventLogNode implements EventProcessorContextListener {
 
     @Getter
     protected EventProcessorContext context;
     @Getter
+    @Setter
+    @FluxtionIgnore
     private String name;
 
     @Initialise
